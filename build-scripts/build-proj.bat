b4e7a9323baa648320580e19a00a0aa93ae4802d
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-06 04:06:21 +0000

    Install minus includes

--- build-scripts/build-proj.bat
+++ build-scripts/build-proj.bat
@@ -1,6 +1,7 @@
 cd ../
 set working_dir=%cd%
 set library_binaries=%working_dir%/library_binaries
+set generator="-G Visual Studio 17 2022"
 
 echo Creating Home for libs
 cd %working_dir%
@@ -8,7 +9,8 @@ if not exist "library_binaries" mkdir library_binaries
 
 if not exist "build" mkdir build
 cd build
-cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -S ./../ -DCMAKE_PREFIX_PATH=%library_binaries%
-cmake --build . --config Release --parallel
+cmake %generator% -A x64 -DCMAKE_BUILD_TYPE=Release -S %working_dir% -B %working_dir%/build
+cmake --build %working_dir%/build --config Release --parallel
+cmake --install %working_dir%/build --prefix %library_binaries% --config Release 
 echo Project built successfully
 pause
