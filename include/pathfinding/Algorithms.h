f11dbe1c42289b8ebf99fca5204e7855b0f2aa6c
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-17 19:11:50 +0000

    removed broken finding Algorithms

--- include/pathfinding/Algorithms.h
+++ include/pathfinding/Algorithms.h
@@ -2,20 +2,33 @@
 #include "../math/Vector2.h"
 #include "Node.h"
 
-namespace ai {
-	namespace path {
+namespace ai
+{
+  namespace path
+  {
+    std::vector<Vector2> A_Star(std::vector<NodePtr> nodes,
+                                NodePtr              start_node,
+                                NodePtr              end_node,
+                                Obstacle             layer);
 
-		struct DLS_Data {
-			NodePtr found;
-			bool remaining;
-		};
+    std::vector<Vector2> BFS(std::vector<NodePtr> nodes,
+                             NodePtr              start_node,
+                             NodePtr              end_node,
+                             Obstacle             layer);
 
-		std::vector<Vector2> A_Star(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer);
-		std::vector<Vector2> BFS(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer);
-		std::vector<Vector2> Greedy_BFS(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer);
-		std::vector<Vector2> DFS(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer);
-		std::vector<Vector2> DLS(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer);
-		std::vector<Vector2> IDDFS_Caller(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer, int depth);
-		std::vector<Vector2> DLS_Caller(std::vector<NodePtr> nodes, NodePtr start_node, NodePtr end_node, Obstacle layer, int depth);
-	} // namespace PATH
-} // namespace AI
+    std::vector<Vector2> Greedy_BFS(std::vector<NodePtr> nodes,
+                                    NodePtr              start_node,
+                                    NodePtr              end_node,
+                                    Obstacle             layer);
+
+    std::vector<Vector2> DFS(std::vector<NodePtr> nodes,
+                             NodePtr              start_node,
+                             NodePtr              end_node,
+                             Obstacle             layer);
+
+    std::vector<Vector2> DLS(std::vector<NodePtr> nodes,
+                             NodePtr              start_node,
+                             NodePtr              end_node,
+                             Obstacle             layer);
+  } // namespace path
+} // namespace ai
