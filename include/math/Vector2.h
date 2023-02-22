cbda129d2cfbbbb9922cee05346afcff87109cfd
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-02-22 01:26:08 +0000

    Added GetAngleBetween functionality

--- include/math/Vector2.h
+++ include/math/Vector2.h
@@ -1,70 +1,130 @@
 #pragma once
 #include <iostream>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
+struct Vector2
+{
+  float x, y;
 
-struct Vector2 {
-	float x, y;
+  Vector2()
+  {
+    x = 0.f, y = 0.f;
+  }
+  Vector2(float x, float y) :
+    x(x), y(y)
+  {
+  }
+  Vector2(int x, int y) :
+    x(float(x)), y(float(y))
+  {
+  }
 
-	Vector2() { x = 0.f, y = 0.f; }
-	Vector2(float x, float y) : x(x), y(y) {}
-	Vector2(int x, int y) : x(float(x)), y(float(y)) {}	
+  friend Vector2 operator+(const Vector2& lhs, const Vector2& rhs)
+  {
+    return Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
+  }
+  friend Vector2 operator-(const Vector2& lhs, const Vector2& rhs)
+  {
+    return Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+  }
+  friend Vector2 operator*(const Vector2& lhs, const Vector2& rhs)
+  {
+    return Vector2(lhs.x * rhs.x, lhs.y * rhs.y);
+  }
+  friend Vector2 operator*(const Vector2& lhs, const float value)
+  {
+    return Vector2(lhs.x * value, lhs.y * value);
+  }
+  friend Vector2 operator/(const Vector2& lhs, const Vector2& rhs)
+  {
+    return Vector2(lhs.x / rhs.x, lhs.y / rhs.y);
+  }
+  friend Vector2 operator/(const Vector2& lhs, float val)
+  {
+    return Vector2(lhs.x / val, lhs.y / val);
+  }
+  friend bool operator==(const Vector2& lhs, const Vector2& rhs)
+  {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+  friend bool operator!=(const Vector2& lhs, const Vector2& rhs)
+  {
+    return !operator==(lhs, rhs);
+  }
+  friend std::ostream& operator<<(std::ostream& out, const Vector2& value)
+  {
+    return out << "(" << value.x << ", " << value.y << ")\n";
+  }
 
-	friend Vector2 operator+(const Vector2& lhs, const Vector2& rhs) { return Vector2(lhs.x + rhs.x, lhs.y + rhs.y); }
-	friend Vector2 operator-(const Vector2& lhs, const Vector2& rhs) { return Vector2(lhs.x - rhs.x, lhs.y - rhs.y); }
-	friend Vector2 operator*(const Vector2& lhs, const Vector2& rhs) { return Vector2(lhs.x * rhs.x, lhs.y * rhs.y); }
-	friend Vector2 operator*(const Vector2& lhs, const float value) { return Vector2(lhs.x * value, lhs.y * value); }
-	friend Vector2 operator/(const Vector2& lhs, const Vector2& rhs) { return Vector2(lhs.x / rhs.x, lhs.y / rhs.y); }
-	friend Vector2 operator/(const Vector2& lhs, float val) { return Vector2(lhs.x / val, lhs.y / val); }
-	friend bool operator==(const Vector2& lhs, const Vector2& rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }
-	friend bool operator!=(const Vector2& lhs, const Vector2& rhs) { return !operator==(lhs, rhs); }
-	friend std::ostream& operator<<(std::ostream& out, const Vector2& value) { return out << "(" << value.x << ", " << value.y << ")\n"; }
+  static float Magnitude(Vector2 vector)
+  {
+    return std::sqrt(vector.x * vector.x + vector.y * vector.y);
+  }
 
-	static float Magnitude(Vector2 vector)
-    {
-        return std::sqrtf(vector.x * vector.x + vector.y * vector.y);
-    }
+  float Magnitude()
+  {
+    return Magnitude(*this);
+  }
 
-	float Magnitude()
-	{
-		return Magnitude(*this);
-	}
+  static Vector2 Normalize(Vector2 vector)
+  {
+    float magnitude = vector.Magnitude(vector);
+    return magnitude > 0 ? Vector2{vector / magnitude} : Vector2{0.f, 0.f};
+  }
 
-	static Vector2 Normalize(Vector2 vector)
-    {
-        float magnitude = vector.Magnitude(vector);
-        return magnitude > 0 ? Vector2{vector / magnitude} : Vector2{0.f,0.f};
-    }
+  Vector2 Normalize()
+  {
+    return Normalize(*this);
+  }
 
-	Vector2 Normalize()
-	{
-		return Normalize(*this);
-	}
+  static float DotProduct(Vector2 vec1, Vector2 vec2)
+  {
+    return vec1.x * vec2.x + vec1.y * vec2.y;
+  }
 
-	static float Distance(Vector2 vec1, Vector2 vec2)
-	{
-		float x = vec2.x - vec1.x;
-		float y = vec2.y - vec1.y;
-		return std::sqrtf(x * x + y * y);
-	}
+  float DotProduct(Vector2 vector)
+  {
+    return DotProduct(*this, vector);
+  }
 
-	float Distance(Vector2 vec)
-	{
-		return Distance(*this, vec);
-	}
+  static float Distance(Vector2 vec1, Vector2 vec2)
+  {
+    float x = vec2.x - vec1.x;
+    float y = vec2.y - vec1.y;
+    return std::sqrt(x * x + y * y);
+  }
 
-	static Vector2 MoveTowards(Vector2 current, Vector2 goal, float move_distance)
-	{	
-		Vector2 vec_to_target = goal - current;
-		float distance_to_target = vec_to_target.Magnitude();
+  float Distance(Vector2 vec)
+  {
+    return Distance(*this, vec);
+  }
 
-		if(distance_to_target < move_distance || distance_to_target == 0.f) return goal;
+  static float GetAngleBetween(Vector2 vec1, Vector2 vec2)
+  {
+    float dx = vec2.x - vec1.x;
+    float dy = vec2.y - vec1.y;
+    float angle = std::atan2(dx, dy);
+    return angle * 180.0 / M_PI;
+  }
 
-		return current + vec_to_target / distance_to_target * move_distance;
-	}
+   float GetAngleBetween(Vector2 vector)
+   {
+     return GetAngleBetween(*this, vector);
+   }
 
-	Vector2 MoveTowards(Vector2 goal, float move_distance)
-	{
-		return MoveTowards(*this, goal, move_distance);
-	}
+  static Vector2 MoveTowards(Vector2 current, Vector2 goal, float move_distance)
+  {
+    Vector2 vec_to_target      = goal - current;
+    float   distance_to_target = vec_to_target.Magnitude();
+
+    if(distance_to_target < move_distance || distance_to_target == 0.f) return goal;
+
+    return current + vec_to_target / distance_to_target * move_distance;
+  }
+
+  Vector2 MoveTowards(Vector2 goal, float move_distance)
+  {
+    return MoveTowards(*this, goal, move_distance);
+  }
 };
