252289295bfe6475b17f4e0f29d2e6c33fb119db
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-04-22 20:26:41 +0100

    Added SearchAlgorithm enum to Algorithms.h

--- include/pf/Algorithms.h
+++ include/pf/Algorithms.h
@@ -6,3 +6,14 @@
 #include "algorithms/DepthLimited.h"
 #include "algorithms/GreedyBestFirst.h"
 #include "algorithms/IterativeDeepeningDepthFirst.h"
+
+enum class SearchAlgorithm : uint8_t
+{
+    A_Star = 0,
+    BestFirst,
+    BiDirectional,
+    DepthFirst,
+    DepthLimited,
+    GreedyBestFirst,
+    IterativeDeepeningDepthFirst
+};
