ed2e6e4d3d5683a310454dad073788b40dc3d066
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-04-24 04:46:29 +0100

    Added CODEOFCONDUCT.md

--- include/pf/algorithms/A_Star.h
+++ include/pf/algorithms/A_Star.h
@@ -5,9 +5,14 @@ namespace ai
 {
   namespace path
   {
+    /**
+     * @brief A simple A_Star implementation
+     *
+     * This is just a test to see how doxygen works.
+     */
     std::vector<Vector2> A_Star(std::vector<NodePtr> nodes,
-                                 NodePtr              start_node,
-                                 NodePtr              end_node,
-                                 Obstacle             layer);
+                                NodePtr              start_node,
+                                NodePtr              end_node,
+                                Obstacle             layer);
   } // namespace path
 } // namespace ai
