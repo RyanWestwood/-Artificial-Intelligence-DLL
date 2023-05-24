5a4b97e8d7b2cdb1cc433f909b2792cc705cf36d
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2023-05-24 03:14:15 +0100

    Added more benchmarks refactored benchmarks/CMakeLists.txt

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+#include <ai/math/Vector2.h>
 #include <ai/pf/Algorithms.h>
 
 static void BM_SmallAStar(benchmark::State& state)
@@ -12,8 +13,70 @@ static void BM_SmallAStar(benchmark::State& state)
     ai::path::Node*      start_node = MAP.at(2 + (2 * MAP_WIDTH));
     ai::path::Node*      goal_node  = MAP.at(5 + (5 * MAP_WIDTH));
     std::vector<Vector2> path       = ai::path::A_Star(MAP, start_node, goal_node, ai::path::Obstacle::None);
+
+
   }
 }
 BENCHMARK(BM_SmallAStar)->Unit(benchmark::kMicrosecond);
+
+static void BM_MediumAStar(benchmark::State& state)
+{
+  constexpr int                MAP_WIDTH  = 100;
+  constexpr int                MAP_HEIGHT = 100;
+  std::vector<ai::path::Node*> MAP        = ai::path::CreateNodeMap(MAP_WIDTH, MAP_HEIGHT);
+
+  for(auto _ : state)
+  {
+    ai::path::Node*      start_node = MAP.at(2 + (2 * MAP_WIDTH));
+    ai::path::Node*      goal_node  = MAP.at(98 + (98 * MAP_WIDTH));
+    std::vector<Vector2> path       = ai::path::A_Star(MAP, start_node, goal_node, ai::path::Obstacle::None);
+  }
+}
+BENCHMARK(BM_MediumAStar)->Unit(benchmark::kMicrosecond);
+
+static void BM_BigAStar(benchmark::State& state)
+{
+  constexpr int                MAP_WIDTH  = 1000;
+  constexpr int                MAP_HEIGHT = 1000;
+  std::vector<ai::path::Node*> MAP        = ai::path::CreateNodeMap(MAP_WIDTH, MAP_HEIGHT);
+
+  for(auto _ : state)
+  {
+    ai::path::Node*      start_node = MAP.at(2 + (2 * MAP_WIDTH));
+    ai::path::Node*      goal_node  = MAP.at(998 + (998 * MAP_WIDTH));
+    std::vector<Vector2> path       = ai::path::A_Star(MAP, start_node, goal_node, ai::path::Obstacle::None);
+  }
+}
+BENCHMARK(BM_BigAStar)->Unit(benchmark::kMillisecond);
+
+static void BM_SuperBigAStar(benchmark::State& state)
+{
+  constexpr int                MAP_WIDTH  = 10000;
+  constexpr int                MAP_HEIGHT = 10000;
+  std::vector<ai::path::Node*> MAP        = ai::path::CreateNodeMap(MAP_WIDTH, MAP_HEIGHT);
+
+  for(auto _ : state)
+  {
+    ai::path::Node*      start_node = MAP.at(2 + (2 * MAP_WIDTH));
+    ai::path::Node*      goal_node  = MAP.at(9998 + (9998 * MAP_WIDTH));
+    std::vector<Vector2> path       = ai::path::A_Star(MAP, start_node, goal_node, ai::path::Obstacle::None);
+  }
+}
+BENCHMARK(BM_SuperBigAStar)->Unit(benchmark::kMillisecond);
+
+static void BM_GameAStar(benchmark::State& state)
+{
+  constexpr int                MAP_WIDTH  = 48;
+  constexpr int                MAP_HEIGHT = 27;
+  std::vector<ai::path::Node*> MAP        = ai::path::CreateNodeMap(MAP_WIDTH, MAP_HEIGHT);
+
+  for(auto _ : state)
+  {
+    ai::path::Node*      start_node = MAP.at(17 + (5 * MAP_WIDTH));
+    ai::path::Node*      goal_node  = MAP.at(24 + (24 * MAP_WIDTH));
+    std::vector<Vector2> path       = ai::path::A_Star(MAP, start_node, goal_node, ai::path::Obstacle::None);
+  }
+}
+BENCHMARK(BM_GameAStar)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
